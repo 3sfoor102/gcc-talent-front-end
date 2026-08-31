7a99700d28982c7b2c8b4ad19979853daf9aaefe
Hasan Ali <hassanalsurh@gmail.com>
2026-08-31 21:15:10 +0300

    Created a service for clients to use to get all jobs posted by themselves

--- src/services/jobs-service.js
+++ src/services/jobs-service.js
@@ -1,26 +1,3 @@
-// const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/jobs`
-// const indexJob = async () => {
-//     try {
-//         const res = await fetch(`${BASE_URL}`, {
-//             method: 'GET',
-//             headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-//         })
-//         const data = await res.json()
-//         if (data.err) {
-//             console.log(data.err)
-//             throw new Error(data.err)
-//         }
-//         return data
-//     } catch (err) {
-//         throw new Error(err)
-//     }
-// }
-// export {
-//     indexJob,
-// }
-
-
-
 import axiosInstance from "./axiosInstance";
 
 const indexJob = async (filters = {}) => {
@@ -41,7 +18,19 @@ const showJob = async (jobId) => {
     return response.data.data;
 };
 
+const getClientJobs = async (filters = {}) => {
+    const response = await axiosInstance.get('/jobs/mine', {
+        params: filters
+    })
+
+    return {
+        data: response.data.data,
+        meta: response.data.meta
+    };
+};
+
 export {
     indexJob,
     showJob,
+    getClientJobs,
 }
