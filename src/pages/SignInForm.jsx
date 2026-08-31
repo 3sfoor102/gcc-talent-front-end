7e7d14ded13659074f261ef490621a5a14a90483
AliAbdulla2002 <alisaleh.contact@gmail.com>
2026-08-31 11:43:51 +0300

    Update signin form with the new design

--- src/pages/SignInForm.jsx
+++ src/pages/SignInForm.jsx
@@ -1,52 +1,79 @@
-import { useNavigate } from "react-router"
 import { useState } from "react"
+import { useNavigate } from "react-router"
 import { signIn } from "../services/auth"
 
-const SignInForm = (props) => {
+const SignInForm = function (props)
+{
 
     const navigate = useNavigate()
 
     const initialState = {
-        username: '',
+        email: '',
         password: '',
     }
+    
     const [formData, setFormData] = useState(initialState)
     const [message, setMessage] = useState('')
+    const [loading, setLoading] = useState(false)
 
-    const handleChange = (event) => {
+    const handleChange = function (event)  
+    {
         setMessage('')
         setFormData({...formData, [event.target.name]: event.target.value})
     }
 
-    const handleSubmit = async (event) => {
+    const handleSubmit = async function (event) 
+    {
         event.preventDefault()
+        setLoading(true)
         try {
             const signedInUser = await signIn(formData)
+
             props.setUser(signedInUser)
+
             setFormData(initialState)
+
             navigate('/')
-        } catch(err) {
+
+        } catch (err) 
+        {
             setMessage(err.message)
+        } finally 
+        {
+            setLoading(false)
         }
     }
 
-    return(
+    return (
         <section className="card">
             <header>
-            <h1>Sign In</h1>
-            <p className="error">{message}</p>
+
+                <h1>Sign In</h1>
+                <p className="error">{message}</p>
+
             </header>
 
             <form onSubmit={handleSubmit}>
-                Username:
-                <input type="text" name="username" value={formData.username} required onChange={handleChange} />
+
+                Email Address:
+                <input type="email" name="email" value={formData.email} required onChange={handleChange} />
+                
                 Password:
                 <input type="password" name="password" value={formData.password} required onChange={handleChange} />
+                
                 <div className="actions">
-                    <button type="submit">Sign In</button>
-                    <button type="button" onClick={() => navigate('/')}>Cancel</button>
+                    <button type="submit" disabled={loading}>
+                        {loading ? 'Signing In...' : 'Sign In'}
+                    </button>
+                    <button type="button" onClick={() => navigate('/sign-up')}>Go to Sign Up</button>
                 </div>
             </form>
+            
+            <hr />
+            <div className="actions">
+                <button type="button">Continue with Google</button>
+                <button type="button">Continue with LinkedIn</button>
+            </div>
         </section>
     )
 }
